7bf22db1571868bf5f56229790fdd529b389d1ef
wzc0x0 <wangzichen@peilian.com>
2019-04-23 17:54:24 +0800

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+/**
+ * Global Blog Config
+ */
+
+module.exports = {
+    title: "Hello VuePress",
+    description: "wzc0x0's blog",
+    head: [
+        ["link", { rel: "icon", href: "/favicon.ico" }]
+    ]
+};
