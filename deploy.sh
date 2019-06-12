d0f954c196affb6ff520a2ce302340210623826f
wzc0x0 <wangzichen@peilian.com>
2019-06-12 17:54:06 +0800

    :sparkles: modifiy deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,21 @@
-
 #!/bin/sh
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
-echo "Generating site"
+# Build the project.
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m ":pencil: Publishing to gh-pages (deploy.sh)" && cd ..
-git push origin gh-pages
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m ":pencil: rebuilding site `date`"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
